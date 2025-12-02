782cdf8af83d1854f1b9588b8cb475074c1ca515
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-12-02 11:36:01 +0545

    feat: update logic to support and fetch station details

--- apps/triggers/src/sources-data/schedule-sources-data.service.ts
+++ apps/triggers/src/sources-data/schedule-sources-data.service.ts
@@ -11,8 +11,6 @@ import {
   DhmWaterLevelAdapter,
   DhmRainfallAdapter,
   DhmService,
-  RiverStationData,
-  RainfallStationData,
 } from '@lib/dhm-adapter';
 import { GlofasAdapter, GlofasServices } from '@lib/glofas-adapter';
 import { GfhAdapter, GfhService } from '@lib/gfh-adapter';
@@ -104,56 +102,12 @@ export class ScheduleSourcesDataService
 
     riverData.data.forEach(async (indicator) => {
       const riverId = (indicator.location as any)?.basinId;
-      // Currently we do not have station data so we are using dummy data
-      const info: RiverStationData = {
-        id: 5554,
-        onm: '',
-        name: riverId,
-        tags: [],
-        basin: 'Mahakali',
-        images: [
-          {
-            id: 2625,
-            name: 'bdca0624990c25570064692a7b807baa',
-            size: 75478,
-            type: 0,
-            description: '',
-          },
-          {
-            id: 2669,
-            name: '8a0a35e1047c0877bef6f16699bbc61f',
-            size: 61510,
-            type: 0,
-            description: '',
-          },
-        ],
-        status: 'BELOW WARNING LEVEL',
-        steady: 'STEADY',
-        district: 'Kanchanpur',
-        latitude: 28.8526,
-        elevation: 162,
-        longitude: 80.4344,
-        series_id: 29089,
-        description: 'Doda (Machheli) river at East West Highway',
-        danger_level: '3.8',
-        stationIndex: '281.5',
-        warning_level: '3.4',
-        waterLevel: {
-          value: indicator.value,
-          datetime: indicator.issuedAt,
-        },
-        history: indicator.info,
-        indicator: indicator.indicator,
-        units: indicator.units,
-        value: indicator.value,
-      };
 
-      const result = await this.dhmService.saveDataInDhm(
+      await this.dhmService.saveDataInDhm(
         SourceType.WATER_LEVEL,
         riverId,
-        info,
+        indicator.info,
       );
-      console.log('result', result);
     });
   }
 
@@ -170,73 +124,17 @@ export class ScheduleSourcesDataService
       } else {
         this.logger.warn(rainfallData.details);
       }
-      // return;
+      return;
     }
 
-    console.log('rainfallData', rainfallData);
-    // Currently rainfall api is not working so we are using dummy data
-    const info: RainfallStationData = {
-      id: 111,
-      name: 'Doda river at East-West Highway',
-      basin: 'Koshi',
-      blink: false,
-      status: 'BELOW WARNING LEVEL',
-      history: [
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T05:00:00.000Z',
-        },
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T06:00:00.000Z',
-        },
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T07:00:00.000Z',
-        },
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T08:00:00.000Z',
-        },
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T09:00:00.000Z',
-        },
-        {
-          max: 0,
-          min: 0,
-          value: 0,
-          datetime: '2025-10-14T10:00:00.000Z',
-        },
-      ],
-      district: 'Sunsari',
-      interval: null,
-      latitude: 26.855192,
-      longitude: 87.152283,
-      series_id: 1505,
-      description: 'Hydrological Station with RLS',
-      stationIndex: '695',
-      indicator: 'water_level_m',
-      units: 'mm',
-      value: 10.9,
-    };
-
-    const result = await this.dhmService.saveDataInDhm(
-      SourceType.RAINFALL,
-      info.name,
-      info,
-    );
-    console.log('result', result);
+    rainfallData.data.forEach(async (indicator) => {
+      const riverId = (indicator.location as any)?.basinId;
+      await this.dhmService.saveDataInDhm(
+        SourceType.RAINFALL,
+        riverId,
+        indicator.info,
+      );
+    });
   }
 
   // run every hour
