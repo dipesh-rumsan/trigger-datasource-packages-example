d84213d7c1949474b916732f5c02bfa274e13ced
Dipesh Kumar Sah <dipesh@rumsan.net>
2025-12-10 15:12:40 +0545

    feat: update glofas algorithm to evaluate trigger statement

--- apps/triggers/src/sources-data/data-source-events.listener.ts
+++ apps/triggers/src/sources-data/data-source-events.listener.ts
@@ -1,8 +1,7 @@
-import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
-import { DATA_SOURCE_EVENTS } from '@lib/core';
-import type { DataSourceEventPayload, Indicator } from '@lib/core';
-import { DataSource, PrismaService } from '@lib/database';
+import * as core from '@lib/core';
+import { DataSource, Prisma } from '@lib/database';
 import { TriggerStatement } from 'src/trigger/validation/trigger.schema';
 import { Parser } from 'expr-eval';
 import { TriggerService } from 'src/trigger/trigger.service';
@@ -11,14 +10,11 @@ import { TriggerService } from 'src/trigger/trigger.service';
 export class DataSourceEventsListener {
   private readonly logger = new Logger(DataSourceEventsListener.name);
 
-  constructor(
-    private prisma: PrismaService,
-    private readonly triggerService: TriggerService,
-  ) {}
+  constructor(private readonly triggerService: TriggerService) {}
 
-  @OnEvent(DATA_SOURCE_EVENTS.DHM.WATER_LEVEL)
-  async handleDhmWaterLevel(event: DataSourceEventPayload) {
-    const indicators: Indicator[] = event.indicators;
+  @OnEvent(core.DATA_SOURCE_EVENTS.DHM.WATER_LEVEL)
+  async handleDhmWaterLevel(event: core.DataSourceEventPayload) {
+    const indicators: core.Indicator[] = event.indicators;
     this.logger.log(
       `DHM WATER LEVEL EVENT RECEIVED ${indicators.length} indicators`,
     );
@@ -62,9 +58,9 @@ export class DataSourceEventsListener {
     }
   }
 
-  @OnEvent(DATA_SOURCE_EVENTS.DHM.RAINFALL)
-  async handleDhmRainfall(event: DataSourceEventPayload) {
-    const indicators: Indicator[] = event.indicators;
+  @OnEvent(core.DATA_SOURCE_EVENTS.DHM.RAINFALL)
+  async handleDhmRainfall(event: core.DataSourceEventPayload) {
+    const indicators: core.Indicator[] = event.indicators;
     this.logger.log(
       `DHM RAIN FALL EVENT RECEIVED ${indicators.length} indicators`,
     );
@@ -109,9 +105,9 @@ export class DataSourceEventsListener {
     }
   }
 
-  @OnEvent(DATA_SOURCE_EVENTS.GLOFAS.WATER_LEVEL)
-  async handleGlofasWaterLevel(event: DataSourceEventPayload) {
-    const indicators: Indicator[] = event.indicators;
+  @OnEvent(core.DATA_SOURCE_EVENTS.GLOFAS.WATER_LEVEL)
+  async handleGlofasWaterLevel(event: core.DataSourceEventPayload) {
+    const indicators = event.indicators;
 
     this.logger.log(
       `GLOFAS WATER LEVEL EVENT RECEIVED ${indicators.length} indicators`,
@@ -121,46 +117,56 @@ export class DataSourceEventsListener {
       this.logger.warn(`indicators not found `);
       return;
     }
+
+    const indicator = indicators[0].indicator;
+
     const triggers = await this.triggerService.findTriggersBySourceAndIndicator(
       DataSource.GLOFAS,
-      indicators[0].indicator,
+      indicator,
     );
 
     if (!triggers.length) {
       this.logger.log('No triggers found for DHM Rainfall event');
       return;
     }
-    /** The indicators freshly emitted from transform() */
 
-    for (const trigger of triggers) {
+    // we will create hash map with key as sourceSubType and values as triggers
+    const triggerMap = triggers.reduce((acc, trigger) => {
       const statement = trigger.triggerStatement as TriggerStatement;
-      const expression = statement.expression;
-
-      // 2. Compute MEAN of all indicator values
-      const meanValue =
-        indicators.reduce((sum, ind) => sum + ind.value, 0) / indicators.length;
-
-      const meetsThreshold = this.evaluateConditionExpression(
-        {
-          expression,
-          sourceSubType: statement.sourceSubType,
-        },
-        meanValue,
-      );
-
-      if (meetsThreshold) {
-        this.logger.log(`Trigger ${trigger.id} MET threshold`);
-        // update trigger
-        await this.triggerService.activateTrigger(trigger.uuid, '', trigger);
-      } else {
-        this.logger.log(`Trigger ${trigger.id} NOT met`);
+      const sourceSubType = statement.sourceSubType;
+      if (!acc[sourceSubType]) {
+        acc[sourceSubType] = [];
       }
+      acc[sourceSubType].push(trigger);
+      return acc;
+    }, {});
+
+    for await (const indicator of indicators) {
+      const [twoYearsMaxProb, fiveYearsMaxProb, twentyYearsMaxProb] =
+        indicator.value.toString().split('/');
+
+      const twoYearsMaxProbTriggers = triggerMap['two_years_max_prob'];
+      const fiveYearsMaxProbTriggers = triggerMap['five_years_max_prob'];
+      const twentyYearsMaxProbTriggers = triggerMap['twenty_years_max_prob'];
+
+      await this.processGlofasTriggers(
+        twoYearsMaxProbTriggers,
+        Number(twoYearsMaxProb.trim()) || 0,
+      );
+      await this.processGlofasTriggers(
+        fiveYearsMaxProbTriggers,
+        Number(fiveYearsMaxProb.trim()) || 0,
+      );
+      await this.processGlofasTriggers(
+        twentyYearsMaxProbTriggers,
+        Number(twentyYearsMaxProb.trim()) || 0,
+      );
     }
   }
 
-  @OnEvent(DATA_SOURCE_EVENTS.GFH.WATER_LEVEL)
-  async handleGfsWaterLevel(event: DataSourceEventPayload) {
-    const indicators: Indicator[] = event.indicators;
+  @OnEvent(core.DATA_SOURCE_EVENTS.GFH.WATER_LEVEL)
+  async handleGfsWaterLevel(event: core.DataSourceEventPayload) {
+    const indicators: core.Indicator[] = event.indicators;
 
     this.logger.log(
       `GFS WATER LEVEL EVENT RECEIVED ${indicators.length} indicators`,
@@ -207,16 +213,27 @@ export class DataSourceEventsListener {
     }
   }
 
+  private generateExpression(triggerStatement: TriggerStatement) {
+    return `${triggerStatement.sourceSubType} ${triggerStatement.operator} ${triggerStatement.value}`;
+  }
+
   private evaluateConditionExpression(
     triggerStatement: { expression: string; sourceSubType: string },
     indicatorValue: number,
   ): boolean {
     try {
-      const parser = new Parser();
+      const parser = new Parser({
+        operators: {
+          logical: true,
+          comparison: true,
+        },
+      });
 
       const variableName = triggerStatement.sourceSubType;
 
-      const exprResult = parser.evaluate(triggerStatement.expression, {
+      const expression = parser.parse(triggerStatement.expression);
+
+      const exprResult = expression.evaluate({
         [variableName]: indicatorValue,
       });
 
@@ -228,5 +245,56 @@ export class DataSourceEventsListener {
       );
       return false;
     }
+  }
+
+  private async processGlofasTriggers(
+    triggers: Prisma.TriggerGetPayload<{
+      include: {
+        phase: true;
+      };
+    }>[] = [],
+    value: number,
+  ) {
+    const activatedTriggers = [];
+    Promise.all(
+      triggers.map(async (trigger) => {
+        const statement = trigger.triggerStatement as TriggerStatement;
+        const expression = this.generateExpression(statement);
+
+        const meetsThreshold = this.evaluateConditionExpression(
+          {
+            expression,
+            sourceSubType: statement.sourceSubType,
+          },
+          value,
+        );
+
+        if (meetsThreshold) {
+          this.logger.log(`Trigger ${trigger.id} MET threshold`);
+          const t = await this.activateTrigger(trigger);
+          activatedTriggers.push(trigger);
+          return t;
+        }
+
+        return null;
+      }),
+    );
+
+    if (activatedTriggers.length > 0) {
+      this.logger.log(
+        `Activated ${activatedTriggers.length} triggers for GLOFAS Subtype ${(triggers[0].triggerStatement as TriggerStatement).sourceSubType}
+         with value ${value} and triggers ${activatedTriggers.map((trigger) => trigger.id).join(', ')}`,
+      );
+    }
+  }
+
+  private async activateTrigger(
+    trigger: Prisma.TriggerGetPayload<{
+      include: {
+        phase: true;
+      };
+    }>,
+  ) {
+    await this.triggerService.activateTrigger(trigger.uuid, '', trigger);
   }
 }
